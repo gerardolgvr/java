2c9abd76881c66e93b1a1aac16cd6b6e7f76d84c
Gerardo Vázquez <gerardolgvr0310@gmail.com>
2020-06-30 20:12:05 -0500

    Regex validate pin code exercise added

---
+++ src/main/java/dev/gerardo/exercises/Pin.java
@@ -0,0 +1,22 @@
+package dev.gerardo.exercises;
+
+import java.util.regex.Pattern;
+
+/* Regex validate PIN code
+ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+
+If the function is passed a valid PIN string, return true, else return false.
+
+eg:
+Solution.validatePin("1234") === true
+Solution.validatePin("12345") === false
+Solution.validatePin("a234") === false
+
+ */
+
+public class Pin {
+
+    public static boolean validate(String pin) {
+        return (Pattern.matches("\\d{4}", pin) || Pattern.matches("\\d{6}", pin))? true : false;
+    }
+}
---
+++ src/test/java/dev/gerardo/exercises/PinTest.java
@@ -0,0 +1,38 @@
+package dev.gerardo.exercises;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PinTest {
+
+    @Test
+    public void validPins() {
+        assertEquals(true, Pin.validate("1234"));
+        assertEquals(true, Pin.validate("0000"));
+        assertEquals(true, Pin.validate("1111"));
+        assertEquals(true, Pin.validate("123456"));
+        assertEquals(true, Pin.validate("098765"));
+        assertEquals(true, Pin.validate("000000"));
+        assertEquals(true, Pin.validate("090909"));
+    }
+
+    @Test
+    public void nonDigitCharacters() {
+        assertEquals(false, Pin.validate("a234"));
+        assertEquals(false, Pin.validate(".234"));
+    }
+
+    @Test
+    public void invalidLengths() {
+        assertEquals(false, Pin.validate("1"));
+        assertEquals(false, Pin.validate("12"));
+        assertEquals(false, Pin.validate("123"));
+        assertEquals(false, Pin.validate("12345"));
+        assertEquals(false, Pin.validate("1234567"));
+        assertEquals(false, Pin.validate("-1234"));
+        assertEquals(false, Pin.validate("1.234"));
+        assertEquals(false, Pin.validate("00000000"));
+    }
+
+}
