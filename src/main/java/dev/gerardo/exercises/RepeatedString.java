93ecd0d43339a5dd1cb0bbea8ac5faf005b0a1e8
Gerardo Vázquez <gerardolgvr0310@gmail.com>
2020-07-04 15:28:47 -0500

    repeated string added"

---
+++ src/main/java/dev/gerardo/exercises/RepeatedString.java
@@ -0,0 +1,29 @@
+package dev.gerardo.exercises;
+
+/* Repeated String
+url: https://www.hackerrank.com/challenges/repeated-string/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+ */
+
+public class RepeatedString {
+    public static long process(String s, long n) {
+        if(s.length() < 1 || s.length() > 100 && (n < 1 && n > Math.pow(10, 12))) {
+            return (long) 0;
+        }
+        int quantityA = 0;
+        long res;
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == 'a') quantityA ++;
+        }
+        long resto = n % (long) s.length();
+        res = n / (long) s.length() * quantityA;
+        if(resto == 0){
+            return res;
+        } else {
+            for(int i = 0; i < resto; i++){
+                if(s.charAt(i) == 'a') res ++;
+            }
+        }
+        return res;
+    }
+
+}
---
+++ src/test/java/dev/gerardo/exercises/RepeatedStringTest.java
@@ -0,0 +1,18 @@
+package dev.gerardo.exercises;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RepeatedStringTest {
+
+    @Test
+    public void test1(){
+        assertEquals(7, RepeatedString.process("aba", (long) 10));
+    }
+
+    @Test
+    public void test2(){
+        assertEquals(1000000, RepeatedString.process("a", (long) 1000000));
+    }
+}
